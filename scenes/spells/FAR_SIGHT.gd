e7498d8b98b4957128369dc18dbff3175c78ab05
sponrad <sponrad@gmail.com>
2021-03-09 14:50:42 -0800

    spell: better swirl for far sight

--- scenes/spells/FAR_SIGHT.gd
+++ scenes/spells/FAR_SIGHT.gd
@@ -5,5 +5,8 @@ onready var game_manager : Node = get_node("/root/MainScene")
 func _ready():
     game_manager.map.clear_fov_at_position(position, 3)
 
+func _process(delta):
+    $Sprite.rotate(delta)
+
 func _on_Timer_timeout():
     queue_free()
